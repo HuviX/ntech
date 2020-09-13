1c2950ba8a393bfd5d287084772520e4f25382ce
huvix <leshamokhov@gmail.com>
2020-09-13 13:24:12 +0300

    function name changed

--- max_subarray.py
+++ max_subarray.py
@@ -19,15 +19,19 @@ def findMaxSubArray(A):
             best_sum = current_sum
             end = i + 1 # +1 нужен, чтобы сделать правильный слайс, т.к. необходимо вернуть элементы до индекса end включительно.
     return A[start: end] #В качестве результата возвращаем слайс с максимальной суммой.
-    
+
 def main(): 
     assert findMaxSubArray([-2,1,-3,4,-1,2,1,-5,4]) == [4, -1, 2, 1]
 
     assert findMaxSubArray([-2, -1]) == [-1]
 
     assert findMaxSubArray([5]) == [5]
-
     print('Ok')
+
+    # test_array = [] #Your Input
+    # ans = findMaxSubArray(test_array)
+    # print(ans)
+    
 
 if __name__ == '__main__':
     main()
