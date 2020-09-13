e787baab74c3b4db383b547df715831493b409b2
huvix <leshamokhov@gmail.com>
2020-09-13 17:52:04 +0300

    inception and resnet34 training scripts added

--- inceptionv3.py
+++ inceptionv3.py
@@ -9,23 +9,24 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 import matplotlib.pyplot as plt
 import time
 import os
+import copy
 import sys
 import pickle
 
 
 
 
-
 data_dir = sys.argv[1]
-model_name = 'resnet34_pretrained'
+
+model_name = 'inceptionv3'
 num_classes = 2
-batch_size = 64
+batch_size = 128
 num_epochs = 100
-input_size = 100 
+input_size = 299 #Необходимо, чтобы размер изображения по наименьшему краю был больше или равен 299.
 feature_extract = True
 
 def model_init():
-    model = models.resnet34(pretrained = True) #загружаем претрен 
+    model = models.inception_v3(pretrained = True, aux_logits=False) #загружаем претрен 
     for param in model.parameters():
             param.requires_grad = False #делаем обучение только для последнего слоя.
     num_ftrs = model.fc.in_features
@@ -35,6 +36,8 @@ def model_init():
 def train_model(model, model_name, dataloaders, criterion, optimizer, scheduler, num_epochs, device):
     val_acc_history = []
     val_loss_history = []
+    best_acc = 0.0
+    model = model.to(device)
     for epoch in range(num_epochs):
         print(f'Epoch {epoch}/{num_epochs-1}')
         for mode in ['train','val']:
@@ -45,42 +48,41 @@ def train_model(model, model_name, dataloaders, criterion, optimizer, scheduler,
             ovr_loss = 0.0
             true_preds = 0
             for image, labels in dataloaders[mode]:
-                images = image.to(device)
+                image = image.to(device)
                 labels = labels.to(device)
                 optimizer.zero_grad()
                 # Если валидация, то делам torch.no_grad()
                 with torch.set_grad_enabled(mode == 'train'): #
-                    outputs = model(images)
+                    outputs = model(image)
                     loss = criterion(outputs, labels)
                     _, preds = torch.max(outputs, dim = 1)
                     if mode == 'train':
                         loss.backward()
                         optimizer.step()
-                ovr_loss += loss.item() * images.size(0)
+                ovr_loss += loss.item() * image.size(0)
                 true_preds += torch.sum(preds == labels.data)
             epoch_loss = ovr_loss/ len(dataloaders[mode].dataset)
             epoch_acc = true_preds.double() / len(dataloaders[mode].dataset)
             
             print(f"{mode} Loss: {epoch_loss}. Accuracy : {epoch_acc}")
-            #Смотрим на функцию потерь на валидации и передаём значение в scheduler.
             if mode == 'val':
-                val_acc_history.append(epoch_acc)
                 val_loss_history.append(epoch_loss)
                 scheduler.step(epoch_loss)
 
-        name = f"todays_model/{model_name}_{epoch}_no_aug"
+        name = f"models/{model_name}_{epoch}"
         torch.save(model.state_dict(), name)
     return model, val_acc_history, val_loss_history
 
 def create_dataloader(data_dir):
+    # Аугментации, чтобы модель была более устойчива к поворотам, отражениям и пр. 
+    # Это также увиличивает размер тренировочной выборки 
     data_transforms = {
     'train': transforms.Compose([
-        transforms.Resize((input_size, input_size)), 
+        transforms.Resize(input_size),
         transforms.RandomResizedCrop(input_size),
-        # transforms.RandomHorizontalFlip(),
-        # transforms.RandomVerticalFlip(),
+        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # нормализация данных
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     ])
     }
     image_dataset = datasets.ImageFolder(data_dir, data_transforms['train'])
@@ -99,42 +101,35 @@ def create_dataloader(data_dir):
 def main():
     model = model_init()
     print("Model Loaded")
+    print(model) #смотрим на архитектуру модели
+    dataloader_dict, device = create_dataloader(data_dir=data_dir)
+    model = model.to(device)
+    params_to_update = model.parameters()
 
-    dataloader_dict, device = create_dataloader(data_dir=data_dir) #создание объекта, который применит к входным данным необходимые преобразование и сможет итерироваться через них
-    model = model.to(device) #перекладываем модель на GPU (если оно есть), чтобы ускорить обучение
-
-    params_to_update = model.parameters() #необходимо получить список параметров модели, чтобы отследить, точно ли будет происходит обучение только последнего слоя
-    print("Params to learn:") 
+    print("Params to learn:")
     params_to_update = []
     for name, param in model.named_parameters():
-        if param.requires_grad:
+        if param.requires_grad == True:
             params_to_update.append(param)
             print("\t",name)
-    #создание оптимизатора SGD с параметром LearningRate = 0.01 и momentum = 0.9
-    optimizer= optim.SGD(params_to_update, lr=0.01, momentum=0.9)
-    #Создание объекта класса ReduceLROnPlateau. Цель - следить за метрикой, которая передается, и если в течении 2х эпох не происходит уменьшение(т.к. mode = 'min'), 
-    # то новое значение lr = lr * 0.1 (factor).
-    scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=2, verbose=True)
-    #Т.к. задача бинарной классификации, то можно минимизировать КроссЭнтропию с целью повышение качества модели (в качестве метрики качества выступает Accuracy)
-    criterion = nn.CrossEntropyLoss()
-    #имя модели, чтобы можно было делать чекпоинты каждую эпоху
-    model_name = "resnet34_"
-    model, acc_hist, loss_hist = train_model(model, model_name, dataloader_dict, criterion, optimizer, scheduler, num_epochs, device)
-
-    # with open('outfile', 'wb') as fp:
-    #     pickle.dump(acc_hist, fp)
-    # with open('outfile2', 'wb') as fp:
-    #     pickle.dump(loss_hist, fp)
-
-    # plt.plot(acc_hist)
-    # plt.savefig('acc_hist.png')
-    # plt.show()
     
-    # plt.plot(loss_hist)
-    # plt.savefig('loss_hist.png')
-    # plt.show()
-    # print("Done")
+    optimizer_ft = optim.SGD(params_to_update, lr=0.01, momentum=0.9)
+    scheduler = ReduceLROnPlateau(optimizer_ft, mode='min', factor=0.1, patience=2, verbose=True)
+    criterion = nn.CrossEntropyLoss()
+    model, acc_hist, loss_hist = train_model(model, model_name, dataloader_dict, criterion, optimizer_ft, scheduler, num_epochs, device)
+    with open('outfile', 'wb') as fp:
+        pickle.dump(acc_hist, fp)
+    with open('outfile2', 'wb') as fp:
+        pickle.dump(loss_hist, fp)
 
+    plt.plot(acc_hist)
+    plt.savefig('acc_hist.png')
+    plt.show()
+    
+    plt.plot(loss_hist)
+    plt.savefig('loss_hist.png')
+    plt.show()
+    print("Done")
 if __name__ == '__main__':
     main()
 
