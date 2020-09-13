5f3ff9d604891ea1b190b6139ecfe6c89c0778df
huvix <leshamokhov@gmail.com>
2020-09-13 13:09:44 +0300

    Problem 1 solution

--- max_subarray.py
+++ max_subarray.py
@@ -1,9 +1,14 @@
+#Используем Kadane's algorithm для нахождений максимального подсписка в списке (сумма элементов максимальна).
+
 def max_array_ind(arr):
     start = 0
     end = 0
-    best_sum = float('-inf')
+    best_sum = float('-inf') # Необходимо, чтобы алгоритм работал, если на вход подаются только отрицательные элементы. Если есть положительные,
+    #то можно присвоить best_sum = 0
     current_sum = 0
-    for i, x in enumerate(arr):
+    #enumerate необходим, чтобы отслеживать индекс элемента и сам элемент. В то время, как в классическом алгоритме необходимо
+    #знать только значение текущего элемента.
+    for i, x in enumerate(arr): 
         if x > current_sum + x:
             start = i
             current_sum = x
@@ -12,11 +17,9 @@ def max_array_ind(arr):
         
         if best_sum < current_sum:
             best_sum = current_sum
-            end = i + 1
-    return arr[start: end]
-
-    
-def main():
+            end = i + 1 # +1 нужен, чтобы сделать правильный слайс, т.к. необходимо вернуть элементы до индекса end включительно.
+    return arr[start: end] #В качестве результата возвращаем слайс с максимальной суммой.
+def main(): 
     assert max_array_ind([-2,1,-3,4,-1,2,1,-5,4]) == [4, -1, 2, 1]
     assert max_array_ind([-2, -1]) == [-1]
     assert max_array_ind([5]) == [5]
